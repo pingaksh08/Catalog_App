c636aa5a7bc974ebc1549e6a5935ebb3d4e68a5d
pingaskh08 <pareekpingaskh08@gmail.com>
2021-05-20 13:46:47 +0530

    DAY 1 COMPLETED.

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Catalog App'),
+        centerTitle: true,
+      ),
+      drawer: Drawer(),
+      
+    );
+  }
+}
---
+++ lib/pages/loginpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  @override
+  _LoginpageState createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loginpage'),
+      
+    );
+  }
+}
